c06a8638ed0593a55eaca29bd91b6d3a64c79b83
burakerturk <ringoust@gmail.com>
2021-03-12 22:31:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Wheather from './WheatherResult'
 import './App.css';
 
+
+
 function App() {
+  const APP_KEY = "6ddfd1077f764ecfa31172921211203"
+  let cityInput = ""
+  const [wheatherData, setWheatherData] = useState([])
+  function CityText() {
+    document.querySelector("input").addEventListener("input", (event) => {
+      event.preventDefault(); // refresh yapmadan çalışacak
+      cityInput = event.target.value;
+      console.log(cityInput);
+
+    })
+  }
+  async function GetData(value) {
+    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no`)
+    const response = await data.json();
+    setWheatherData(response.forecast.forecastday)
+    console.log(response.forecast.forecastday)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="search">
+        <input type="text" placeholder="Search a city" onChange={CityText} />
+        <button onClick={() => GetData(cityInput)}>Search</button>
+      </div>
+      {wheatherData.map(item => (<Wheather key={item.date} date={item.date} minTemp={item.day.mintemp_c}
+        maxTemp={item.day.maxtemp_c} condition={item.day.condition.text} icon={item.day.condition.icon}/>
+        ))};
     </div>
+
   );
 }
 
